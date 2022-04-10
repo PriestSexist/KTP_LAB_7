d82a3fb1b5a32f002ac621e8e927413f55362160
Viktor <vitekb650@gmail.com>
2022-04-10 13:34:51 +0300

    коммит
    коммит

---
+++ Lab_7-8/src/com/company/Test.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Test {
+    public static void main(String[] args) {
+        for(String str : args) {
+            System.out.println(str);
+        }
+    }
+}
